45bd61b201bcff9a1c9d304c52877ffa9169750a
dan.perocheau <dperocheau@ippon.fr>
2024-09-16 10:54:44 +0200

    feat: initial project commit

---
+++ dataPersistence/dataPersistenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  dataPersistenceApp.swift
+//  dataPersistence
+//
+//  Created by Dan PEROCHEAU on 16/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct dataPersistenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
